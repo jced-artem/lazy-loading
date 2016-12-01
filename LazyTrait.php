9d3dded854fd97b8a3d796fa2eb2739ab3438e80
Artem <artem.jcd@gmail.com>
2016-12-01 18:45:47 +0200

    Update LazyTrait.php

--- LazyTrait.php
+++ LazyTrait.php
@@ -8,18 +8,18 @@ namespace Jced;
 trait LazyTrait
 {
     /**
-     * @param mixed $result
-     * @param \Closure $closure
+     * @param mixed $property
+     * @param mixed $return
      * @param bool $triggerEmpty
      * @param bool $triggerNull
      * @param bool $triggerFalse
      * @return mixed
      */
-    public function lazy(&$result, \Closure $closure, $triggerEmpty = true, $triggerNull = false, $triggerFalse = false)
+    public function lazy(&$property, $return, $triggerEmpty = true, $triggerNull = false, $triggerFalse = false)
     {
-        if ($triggerEmpty && empty($result) or $triggerNull && is_null($result) or $triggerFalse && $result === false) {
-            $result = $closure();
+        if ($triggerEmpty && empty($property) or $triggerNull && is_null($property) or $triggerFalse && $property === false) {
+            $property = is_callable($return) ? $return() : $return;
         }
-        return $result;
+        return $property;
     }
 }
